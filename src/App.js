f56dc96c023ba1ec0ab03b91ba5f1a759118096c
Cho Suhyeon <ckc020506@gmail.com>
2025-06-04 16:21:24 +0900

    feat: Route 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, Link } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import MainTeacher from "./pages/MainTeacher";
-import MainStudent from "./pages/MainStudent";
 import AddQuestion from "./pages/AddQuestion";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/teacher/main" element={<MainTeacher />} />
-        <Route path="/student/main" element={<MainStudent />} />
         <Route path="/teacher/add-question" element={<AddQuestion />} />
       </Routes>
 
--- src/pages/MainStudent.js
+++
@@ -1,9 +0,0 @@
-const MainStudent = () => {
-return(
-    <div>
-        MainStudent 페이지입니다.
-    </div>
-);
-};
-
-export default MainStudent;
